721248c4254f0d39bf44b3ee2259fdc53f164c58
Andrew Luca <thendrluca@gmail.com>
2020-05-08 06:31:02 +0300

    feat: accept a URL parameter for helpers using location

--- source/utils.ts
+++ source/utils.ts
@@ -2,18 +2,18 @@ import {isRepo} from '.';
 
 export const getUsername = () => document.querySelector('meta[name="user-login"]')!.getAttribute('content')!;
 
-export const getDiscussionNumber = (): string | undefined => location.pathname.split(/(?:pull|issues)\/(\d+)/)[1];
+export const getDiscussionNumber = (url: URL | Location = location): string | undefined => url.pathname.split(/(?:pull|issues)\/(\d+)/)[1];
 
 // Drops leading and trailing slash to avoid /\/?/ everywhere
-export const getCleanPathname = (): string => location.pathname.replace(/^\/|\/$/g, '');
+export const getCleanPathname = (url: URL | Location = location): string => url.pathname.replace(/^\/|\/$/g, '');
 
 // Parses a repo's subpage, e.g.
 // '/user/repo/issues/' -> 'issues'
 // '/user/repo/' -> ''
 // returns undefined if the path is not a repo
-export const getRepoPath = (): string | undefined => {
-	if (isRepo()) {
-		return getCleanPathname().split('/').slice(2).join('/');
+export const getRepoPath = (url: URL | Location = location): string | undefined => {
+	if (isRepo(url)) {
+		return getCleanPathname(url).split('/').slice(2).join('/');
 	}
 
 	return undefined;
