63005a47d73d261f33162d85f0144a0767844d2e
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-07-07 16:59:38 -0400

    Add `isPRFile404` (#20)

--- test.ts
+++ test.ts
@@ -111,6 +111,16 @@ test('isPRCommit404', t => {
 	t.false(pageDetect.isPRCommit404());
 });
 
+test('isPRFile404', t => {
+	document.title = 'Commit range not found · Pull Request #789 · sindresorhus/eslint-plugin-unicorn';
+	location.href = 'https://github.com/sindresorhus/eslint-plugin-unicorn/pull/789/files/a58b37845f1b2660221de019e4ae6c736feedc26..eed168224d7994652b1d1ff69a5c8cebee223faf';
+	t.true(pageDetect.isPRFile404());
+
+	document.title = 'Add `align-repository-header` feature by fregante · Pull Request #3313 · sindresorhus/refined-github';
+	location.href = 'https://github.com/sindresorhus/refined-github/pull/3313/files/a14fb2c94eae3ca83a3a97688a171fcc3405524f..fbeeba9825f12b5ded9cd4bb04d5df4b0cf2f2a8';
+	t.false(pageDetect.isPRFile404());
+});
+
 test('getRepoPath', t => {
 	const pairs = new Map<string, string | undefined>([
 		[
