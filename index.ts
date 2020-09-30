41dff2906b11391c0f6b277478f4672039ec15fc
Yakov <16872793+yakov116@users.noreply.github.com>
2020-09-30 16:24:53 -0400

    Drop depreciated

--- index.ts
+++ index.ts
@@ -419,10 +419,6 @@ collect.set('isSingleTag', [
 	'https://github.com/sindresorhus/refined-github/releases/tag/0.2.1',
 ]);
 
-/** @deprecated use isSingleTag instead */
-export const isSingleTagPage = isSingleTag;
-collect.set('isSingleTagPage', combinedTestOnly);
-
 collect.set('hasComments', combinedTestOnly);
 export const hasComments = (url: URL | Location = location): boolean =>
 	isPR(url) ||
@@ -451,14 +447,6 @@ collect.set('isMarketplaceAction', [
 	'https://github.com/marketplace/actions/github-action-for-assignee-to-reviewer',
 	'https://github.com/marketplace/actions/hugo-actions',
 ]);
-
-/** @deprecated use isMarketplaceAction instead */
-export const isActions = isMarketplaceAction;
-collect.set('isActions', combinedTestOnly);
-
-/** @deprecated use isActions instead */
-export const isActionPage = isActions;
-collect.set('isActionPage', combinedTestOnly);
 
 export const isActionJobRun = (url: URL | Location = location): boolean => String(getRepoPath(url)).startsWith('runs/');
 collect.set('isActionJobRun', [
