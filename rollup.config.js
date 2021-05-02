6bcef91113272ab8c34a4b7076e91a3dc33b8843
Federico <me@fregante.com>
2021-05-02 10:42:42 +0700

    Lint

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import typescript from '@rollup/plugin-typescript';
 import {terser} from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
-export default {
+const config = {
 	input: 'index.ts',
 	output: {
-		'esm',
+		format: 'esm',
 		dir: 'esm',
 	},
 	plugins: [
@@ -35,3 +35,5 @@ export default {
 		),
 	],
 };
+
+export default config;
