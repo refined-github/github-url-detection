0ea3a01afdc441e5ac39748a2b022a25353d9b8e
port19 <port19@port19.xyz>
2023-09-12 11:12:40 +0200

    appease the linter

--- index.ts
+++ index.ts
@@ -82,7 +82,7 @@ addTests('isDashboard', [
 	'https://github.com/?tab=overview', // Gotcha for `isUserProfileMainTab`
 	'https://github.com?search=1', // Gotcha for `isRepoTree`
 	'https://github.com/dashboard-feed',
-	'https://github.com//dashboard-feed'
+	'https://github.com//dashboard-feed',
 ]);
 
 export const isEnterprise = (url: URL | HTMLAnchorElement | Location = location): boolean => url.hostname !== 'github.com' && url.hostname !== 'gist.github.com';
