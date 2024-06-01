e3b918cab45bff0c8c6ffbf97869c155306be14a
Federico <me@fregante.com>
2024-06-01 12:38:49 +0800

    Don't bundle github-reserved-names

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,11 @@ export default defineConfig({
 			entry: 'index.ts',
 			fileName: 'index',
 		},
-		target: 'es2021',
+		target: 'node20.10', // https://github.com/evanw/esbuild/issues/3778
 		outDir: 'distribution',
 		minify: false,
+		rollupOptions: {
+			external: ['github-reserved-names/reserved-names.json'],
+		},
 	},
 });
