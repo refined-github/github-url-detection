69e5afe167ccb4c4856d20c06d5e72d0a8e0be04
Grigory <techuila.sunset@gmail.com>
2026-02-01 05:14:08 +0500

    `isQuickPR` - Support 'expand' query parameter

--- index.ts
+++ index.ts
@@ -288,11 +288,12 @@ TEST: addTests('isPRFiles', [
 	'https://github.com/refined-github/refined-github/pull/148/changes/1e27d7998afdd3608d9fc3bf95ccf27fa5010641..e1aba6febb3fe38aafd1137cff28b536eeeabe7e',
 ]);
 
-export const isQuickPR = (url: URL | HTMLAnchorElement | Location = location): boolean => isCompare(url) && /[?&]quick_pull=1(&|$)/.test(url.search);
+export const isQuickPR = (url: URL | HTMLAnchorElement | Location = location): boolean => isCompare(url) && /[?&](quick_pull|expand)=1(&|$)/.test(url.search);
 TEST: addTests('isQuickPR', [
 	'https://github.com/sindresorhus/refined-github/compare/master...branch-name?quick_pull=1',
 	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2?quick_pull=1',
 	'https://github.com/sindresorhus/refined-github/compare/test-branch?quick_pull=1',
+	'https://github.com/refined-github/sandbox/compare/default-a...fregante-patch-2?expand=1'
 ]);
 
 const getStateLabel = (): string | undefined => $([
