4d0de6357706724fdf9d0b2e61009194c6c281a3
Grigory <techuila.sunset@gmail.com>
2026-02-17 16:40:31 +0500

    test urls

--- index.ts
+++ index.ts
@@ -70,14 +70,12 @@ TEST: addTests('isDashboard', [
 	'https://github.com//',
 	'https://github.com/',
 	'https://github.com',
-	'https://github.com/orgs/test/dashboard',
+	'https://github.com/orgs/refined-github/dashboard',
 	'https://github.com/dashboard/index/2',
 	'https://github.com//dashboard',
 	'https://github.com/dashboard',
-	'https://github.com/orgs/edit/dashboard',
-	'https://github.big-corp.com/',
-	'https://not-github.com/',
-	'https://my-little-hub.com/',
+	'https://ghe.big-corp.com/',
+	'https://ghe-instance.com/',
 	'https://github.com/?tab=repositories', // Gotcha for `isUserProfileRepoTab`
 	'https://github.com/?tab=stars', // Gotcha for `isUserProfileStarsTab`
 	'https://github.com/?tab=followers', // Gotcha for `isUserProfileFollowersTab`
@@ -90,7 +88,19 @@ TEST: addTests('isDashboard', [
 export const isHome = (url: URL | HTMLAnchorElement | Location = location): boolean => !isGist(url) && /^$|^dashboard\/?$/.test(getCleanPathname(url));
 TEST: addTests('isHome', [
 	'https://github.com',
+	'https://github.com///',
+	'https://github.com//',
+	'https://github.com/',
+	'https://github.com//dashboard',
 	'https://github.com/dashboard',
+	'https://ghe.big-corp.com/',
+	'https://ghe-instance.com/',
+	'https://github.com/?tab=repositories', // Gotcha for `isUserProfileRepoTab`
+	'https://github.com/?tab=stars', // Gotcha for `isUserProfileStarsTab`
+	'https://github.com/?tab=followers', // Gotcha for `isUserProfileFollowersTab`
+	'https://github.com/?tab=following', // Gotcha for `isUserProfileFollowingTab`
+	'https://github.com/?tab=overview', // Gotcha for `isUserProfileMainTab`
+	'https://github.com?search=1', // Gotcha for `isRepoTree`
 ]);
 
 export const isFeed = (url: URL | HTMLAnchorElement | Location = location): boolean => !isGist(url) && /^(feed|orgs\/[^/]+\/dashboard)\/?$/.test(getCleanPathname(url));
